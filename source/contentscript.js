dbae550543906cf305448c7fd5ad0acb6f556019
pureth <pureth@googlemail.com>
2018-02-18 21:06:13 +0000

    linting and adding test.html

--- source/contentscript.js
+++ source/contentscript.js
@@ -1,159 +1,177 @@
-
-
-
-//define the name of the storage variable. 
+//define the name of the storage variable.
 var storageNamespace = "lolkeg_data";
 
-//checks browser storage for previously saved data 
+//checks browser storage for previously saved data
 //and if it doesnt exist we define a default.
-if (!localStorage[storageNamespace]){
-    localStorage[storageNamespace] = JSON.stringify({
-        "lorem": "hats hats hats far away.",
-        "ipsum": "pah babababababa babbababa. \n\n\n http://soapbox.github.io/linkifyjs/docs/"
-    });
+if (!localStorage[storageNamespace]) {
+  localStorage[storageNamespace] = JSON.stringify({
+    lorem: "hats hats hats far away.",
+    ipsum:
+      "pah babababababa babbababa. \n\n\n http://soapbox.github.io/linkifyjs/docs/"
+  });
 }
+
+/*
+*
+*
+*
+* The plan:
+* split dictionary keys into an array of words.
+* sort dictionary by key array length
+* check for a match of the first word of the key's word array.
+* if match found on first word, check rest.
+*
+*
+*
+*/
 
 //gets the data from browser storage and converts it from a string to a javascript object
 var data = JSON.parse(localStorage[storageNamespace]);
-
-
 
 //this code loops through the entire DOM, looking for html elements/ text elements
 //and within those it looks for words matching the uploaded data set
 //and then once it's found matching words they are all added to the: pairs array
 //and once the whole DOM has been search the DOM is updated again based the pairs array
 //The function is anonymous, so the code is self contained. It's executed onces when the window loads
-(function(){
+(function() {
+  var elements = document.getElementsByTagName("*");
+  var pairs = [];
+  var timeout = 0;
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
 
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+        console.log(text);
+        var textArray = text.split(" ");
+        var replacedArray = [];
+        var changed = false;
 
-    var elements = document.getElementsByTagName('*');
-    var pairs = [];
-    var timeout = 0;
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-
-        for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
-            if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                
-                var textArray = text.split(" ");
-                var replacedArray = []
-                var changed = false;
-
-                for (var k = 0; k < textArray.length; k++) {
-                    replacedArray.push(textArray[k]);
-                    for (var key in data) {
-                        var str = textArray[k].toLowerCase().replace(/^[a-z ]$/i, '');
-                        if (((str.indexOf(key) === 0) || (str === key+"s")) && (str.length <= key.length + 1)) {
-                            replacedArray[k] = "<span class='lolkeg' keyword='"+key+"'>" + textArray[k] + "</span>";
-                            changed = true;
-                            break;
-                        }
-                    }
-                }
-                var replacedText = replacedArray.join(" ");
-                
-                if (changed === true) {
-                    pairs.push({element: element, old: node, new: "<span>"+replacedText+"</span>"});
-                }
+        for (var k = 0; k < textArray.length; k++) {
+          replacedArray.push(textArray[k]);
+          for (var key in data) {
+            var str = textArray[k].toLowerCase().replace(/^[a-z ]$/i, "");
+            if (
+              (str.indexOf(key) === 0 || str === key + "s") &&
+              str.length <= key.length + 1
+            ) {
+              replacedArray[k] =
+                "<span class='lolkeg' keyword='" +
+                key +
+                "'>" +
+                textArray[k] +
+                "</span>";
+              changed = true;
+              break;
             }
+          }
         }
+        var replacedText = replacedArray.join(" ");
+
+        if (changed === true) {
+          pairs.push({
+            element: element,
+            old: node,
+            new: "<span>" + replacedText + "</span>"
+          });
+        }
+      }
     }
-
-    for (var j = 0; j < pairs.length; j++) {
-        pairs[j].element.replaceChild($( pairs[j].new )[0], pairs[j].old);
-    }
-
-
-
+  }
+  console.log(pairs[0]);
+  for (var j = 0; j < pairs.length; j++) {
+    pairs[j].element.replaceChild($(pairs[j].new)[0], pairs[j].old);
+  }
 })();
 
-
 //listens for new data received when a new file is uploaded in options.js
-//and when it receives the data saves it to the browser storage so i can
+//and when it receives the data saves it to the browser storage so it can
 //be accessed next time the browser window is refreshed / loaded
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+if (
+  chrome &&
+  chrome.runtime &&
+  chrome.runtime.onMessage &&
+  chrome.runtime.onMessage.addListener
+) {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     localStorage[storageNamespace] = request.data;
-});
-
-
+  });
+}
 
 //look into the DOM for the body element and any .lolkeg classes (which are added to words for highlighting)
-var body = $( "body", document );
-var words = $( ".lolkeg" );
+var body = $("body", document);
+var words = $(".lolkeg");
 
-//create a new DOM element with a class called masterkey 
+//create a new DOM element with a class called masterkey
 //for holding the the word descriptions
-let holder = $( "<div class='masterkeg'></div>" );
+let holder = $("<div class='masterkeg'></div>");
 body.append(holder);
 
 // this function positions the holders to be next to the mouse,
-//and also if the mouse is in the left hand of the screen, 
+//and also if the mouse is in the left hand of the screen,
 //the older will be to the right of the mouse, and vice versa
-words.mouseover(function(event){
-    clearTimeout(timeout);
-    var left = (event.pageX);
-    var top = (event.pageY);
-    if ($(window).width() / 2 < event.pageX){
-        left -= 300;
-    }
-    
-    //get the keyword from the html word element and use it to
-    //to get the description of the same word from the data var
-    var key = $(event.target).attr("keyword");
-    var text = "";
-    text += titleCase(key);//add the keyword to the top in uppercase
-    text += "<br>";//add linebreak
-    text += data[key].replace(/\n/g, "<br>");//add the description with any \n turned into html linebreaks
-    holder.html(text);//set the text to be inside the holder element
-    
-    //add position and display to block, which makes the holder visible again
-    holder.css({
-        display: "block",
-        top: top + "px",
-        left: left + "px",
-    });
+words.mouseover(function(event) {
+  clearTimeout(timeout);
+  var left = event.pageX;
+  var top = event.pageY;
+  if ($(window).width() / 2 < event.pageX) {
+    left -= 300;
+  }
 
-    //turn any URLs in the holder into a Hyperlink (uses the likify plugin in the /lib/ folder)
-    holder.linkify({
-        target: "_blank"
-    });
+  //get the keyword from the html word element and use it to
+  //to get the description of the same word from the data var
+  var key = $(event.target).attr("keyword");
+  var text = "";
+  text += titleCase(key); //add the keyword to the top in uppercase
+  text += "<br>"; //add linebreak
+  text += data[key].replace(/\n/g, "<br>"); //add the description with any \n turned into html linebreaks
+  holder.html(text); //set the text to be inside the holder element
 
+  //add position and display to block, which makes the holder visible again
+  holder.css({
+    display: "block",
+    top: top + "px",
+    left: left + "px"
+  });
+
+  //turn any URLs in the holder into a Hyperlink (uses the likify plugin in the /lib/ folder)
+  holder.linkify({
+    target: "_blank"
+  });
 });
 
 //when the mouse goes over the holder, make sure the timeout is removed
-holder.mouseover(function(event){
-    clearTimeout(timeout);
+holder.mouseover(function(event) {
+  clearTimeout(timeout);
 });
 
 //when the mouse leaves either the holder or a word, start the timeout to hide the holder
 words.mouseout(startTimeout);
 holder.mouseout(startTimeout);
 
-
-//define a timeout variable and a function that creates a timeout 
+//define a timeout variable and a function that creates a timeout
 //which when triggered will hide the holder / .masterkeg element
 var timeout;
-function startTimeout(){
-    clearTimeout(timeout);
-    timeout = setTimeout(function(){
-        holder.css({
-            display: "none"
-        });
-    }, 1000)
+function startTimeout() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    holder.css({
+      display: "none"
+    });
+  }, 1000);
 }
-
-
 
 //this function makes the first letter of each word in a string turn to upper case
 function titleCase(str) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-    }
-    // Directly return the joined string
-    return splitStr.join(' '); 
- }
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
--- source/options/options.js
+++ source/options/options.js
@@ -1,19 +1,19 @@
 //when the options page has fully loaded / is ready
 //we look for changes to the #file id / element
 //and when we see a file added, we load it.
-$(document).ready(function(){
-  $("#file").change(function () {
-		var file = document.getElementById('file').files[0];
-		if(file){
-			loadFile(file);
-		}
+$(document).ready(function() {
+  $("#file").change(function() {
+    var file = document.getElementById("file").files[0];
+    if (file) {
+      loadFile(file);
+    }
   });
 });
 
 //load the file and read it's contents and
 //assign functions to the different possible events
 function loadFile(readFile) {
-  var reader = new FileReader();  
+  var reader = new FileReader();
   reader.readAsText(readFile);
   // Handle progress, success, and errors
   reader.onprogress = updateProgress;
@@ -26,7 +26,7 @@ function loadFile(readFile) {
 function updateProgress(event) {
   if (event.lengthComputable) {
     // event.loaded and event.total are ProgressEvent properties
-    var loaded = (event.loaded / event.total);
+    var loaded = event.loaded / event.total;
     if (loaded < 1) {
       // Increase the prog bar length
       // style.width = (loaded * 200) + "px";
@@ -37,19 +37,19 @@ function updateProgress(event) {
 //when the file has been loaded we get the data from the event parameter
 //and parse it to the emitData function to be sent to the contentscript.js file
 //and then updates the feedback element with a success message
-function loaded(event) {  
-	var fileString = event.target.result;
-	emitData(fileString);
+function loaded(event) {
+  var fileString = event.target.result;
+  emitData(fileString);
   $("#feedback").html("New file successfully uploaded.");
-  $("#feedback").removeClass();//remove all old classes
+  $("#feedback").removeClass(); //remove all old classes
   $("#feedback").addClass("success");
 }
 
 //provide some basic feedback for when the upload fails
 function errorHandler(event) {
-  if(event.target.error.name == "NotReadableError") {
+  if (event.target.error.name == "NotReadableError") {
     $("#feedback").html("There was an error uploading your file.");
-    $("#feedback").removeClass();//remove all old classes
+    $("#feedback").removeClass(); //remove all old classes
     $("#feedback").addClass("error");
   }
 }
@@ -57,10 +57,10 @@ function errorHandler(event) {
 //this function receives the data from the file and then
 //sends it to the any scripts that are listening (contentscript.js)
 //the response is currently only logged to the console, nothing else is done with it.
-function emitData(data){
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {data: data}, function(response) {
-			console.log(response);
-		});
-	});
+function emitData(data) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { data: data }, function(response) {
+      console.log(response);
+    });
+  });
 }
